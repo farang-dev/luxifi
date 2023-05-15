458d9df9bc8021f4c94f6f4ef40b09f36f31237b
Anne Garvey <amgarvey15@gmail.com>
2023-05-15 17:00:05 +0900

    Added validations to item and booking class

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  validate :start_date, :end_date, presence: true
+  CATEGORY = ["approved", "pending", "rejected"]
+  validate :status, inclusion: { in: CATEGORY }
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :user, through: :bookings
+  validates :brand, :price, :category, :name, :gender, presence: true
 end
