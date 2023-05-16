ade0b3657f34798718e43bc4fa1ae67bb47112e5
Cai Dixon <city.art.girl@gmail.com>
2023-05-16 11:42:02 +0900

    added authorization to bookings

--- app/policies/bookings_policy.rb
+++ app/policies/bookings_policy.rb
@@ -1,4 +1,29 @@
 class BookingsPolicy < ApplicationPolicy
+
+  def index?
+    @record.user == user
+  end
+
+  def show?
+    @record.user == user
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    @record.user == user
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    @record.user == user
+  end
+
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
