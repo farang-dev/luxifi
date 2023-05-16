de234f01b73f15ba0508ccc99c97a6177f178a24
Cai Dixon <city.art.girl@gmail.com>
2023-05-16 11:35:36 +0900

    added authorization to items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,17 @@
 class ItemsController < ApplicationController
   def index
+    authorize @items
   end
 
   def show
+    authorize @item
   end
 
   def new
+    authorize @item
   end
 
   def create
+    authorize @item
   end
 end
