1859dcc75cae3ff68bb331faba7df100098503e1
Anne Garvey <amgarvey15@gmail.com>
2023-05-18 12:00:25 +0900

    items controller create and new

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,10 +10,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
     authorize(@item)
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.user = current_user
     authorize(@item)
+    if
+      @item.save
+      redirect_to bookings_path(current_user)
+    else
+      render :new, status, :unprocessable_entity
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:category, :brand, :price, :name, :gender, :comment, :tags)
   end
 end
