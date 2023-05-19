a319ac4d59bc26d928da8d892747101937066bc5
Anne Garvey <amgarvey15@gmail.com>
2023-05-19 12:27:30 +0900

    added stuff to the form.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,10 @@ class Booking < ApplicationRecord
   belongs_to :item
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true
   STATUS = ["approved", "pending", "rejected"]
   validates :status, inclusion: { in: STATUS }
 end
